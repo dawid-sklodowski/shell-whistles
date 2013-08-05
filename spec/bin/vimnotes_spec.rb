b52680c8b407129329306998f37afaec80a4fb98
Dawid Sklodowski <davesqu@gmail.com>
2013-08-05 23:00:53 +0100

    Tests finished

--- spec/bin/vimnotes_spec.rb
+++ spec/bin/vimnotes_spec.rb
@@ -1,14 +1,15 @@
 require 'fileutils'
+require 'pry'
 ENV['VIMNOTES_ENV'] = 'test'
 describe 'vimnotes' do
-  ROOT = Pathname.new(File.expand_path(__FILE__)) + '..' + '..'
+  ROOT = Pathname.new(File.expand_path(__FILE__)) + '..' + '..' + '..'
   TEST_DIR = ROOT + 'test_tmp'
 
-  def run(command)
-    `#{ROOT + 'bin' + 'vimnotes'} -d #{TEST_DIR} #{command}`
+  def run(options)
+    `#{ROOT + 'bin' + 'vimnotes'} -d #{TEST_DIR} #{options}`
   end
 
-  after(:each) do
+  before(:each) do
     FileUtils.rm_rf TEST_DIR
   end
 
@@ -19,24 +20,30 @@ describe 'vimnotes' do
   end
 
   it 'creates new file with today date if no exists' do
-
+    run('test_file').should =~ /test_file-#{Time.now.strftime('%Y-%m-%d')}/
   end
 
   it 'opens existing file if it exists' do
-
+    TEST_DIR.mkpath
+    (TEST_DIR + 'test_file-2010-04-27.txt').open('w') { |file| file << 'test' }
+    run('test_file').should =~ /test_file-2010-04-27\.txt/
   end
 
   describe '-n option' do
     it 'creates new file if none exist' do
-
+      run('-n test_file').should =~ /test_file-#{Time.now.strftime('%Y-%m-%d')}/
     end
 
     it 'it creates new file with today date if file exists with previous date' do
-
+      TEST_DIR.mkpath
+      (TEST_DIR + 'test_file-2010-04-27.txt').open('w') { |file| file << 'test' }
+      run('-n test_file').should =~ /test_file-#{Time.now.strftime('%Y-%m-%d')}.txt/
     end
 
     it 'creates new file with today timestamp if file exists with today date' do
-
+      TEST_DIR.mkpath
+      (TEST_DIR + "test_file-#{Time.now.strftime('%Y-%m-%d')}.txt").open('w') { |file| file << 'test' }
+      run('-n test_file').should =~ /test_file-#{Time.now.strftime('%Y-%m-%d')}_\d{2}\.\d{2}\.\d{2}\.txt/
     end
   end
 end
